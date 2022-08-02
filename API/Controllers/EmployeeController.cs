5de30da2fe3ee21a8379d5256e190af237989d93
chandan <cgiri3292@gmail.com>
2022-08-02 19:11:45 +0530

    Added GET and POST API

---
+++ API/Controllers/EmployeeController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+using API.Application;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EmployeeController : ControllerBase
+    {
+
+        private readonly ILogger<EmployeeController> _logger;
+
+        public EmployeeController(ILogger<EmployeeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Get(string id)
+        {
+            var emp = new EmployeeModel();
+
+            emp = emp.FetchEmployeeDetails(id);
+
+            if(emp!=null)
+            {
+                return Ok(emp);
+            }
+            else
+            {
+                return Ok("ID Not Found!");
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] EmployeeModel model)
+        {
+            var emp = new EmployeeModel();
+            
+            string json = JsonSerializer.Serialize(model);
+            emp.SaveEmployeeDetails(json);
+
+            return Ok("Employee Details Saved Successfully!");
+        }
+    }
+}
